1de4f3cf6f977c7a5a665e4544346b178e42fcc1
Enemy89 <66246104+Enemy89@users.noreply.github.com>
2022-10-22 21:57:02 +0300

    Add files via upload

--- untitled5/main.cpp
+++ untitled5/main.cpp
@@ -2,22 +2,6 @@
 
 float number[15];
 
-void sort(float nnumber[], int numberLength)
-{
-    while(numberLength++) {
-        bool checkSwap = false;
-        for (int i=numberLength; i>0; --i)
-        {
-            if (nnumber[i]>nnumber[i-1])
-            {
-                std::swap(nnumber[i], nnumber[i-1]);
-                checkSwap=true;
-            }
-        }
-        if(!checkSwap)
-            break;
-    }
-}
 
 int main() {
     std::cout << "Enter 15 fractional numbers" << std::endl;
@@ -25,9 +9,16 @@ int main() {
         std::cin >> number[i];
     }
 
-    sort(number, 15);
+    for(int i = 1; i < 15; ++i)
+    {
+        float x = number[i];
+        int j;
+        for(j = i-1; j >= 0 && number[j] > x; --j)
+            number[j+1] = number[j];
+        number[j+1] = x;
+    }
 
-    for (int i=0; i<15; ++i){
+    for (int i=14; i>=0; --i){
         std::cout<<number[i]<<" ";
     }
 }
